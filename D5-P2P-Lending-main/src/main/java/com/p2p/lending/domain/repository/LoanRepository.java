b2891b28055956620beb9dd3322a4c5c73567018
LuthfianaAriba <luthfiana.ariba.tif425@polban.ac.id>
2026-05-20 09:13:08 +0700

    Delete D5-P2P-Lending-main directory

--- D5-P2P-Lending-main/src/main/java/com/p2p/lending/domain/repository/LoanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.p2p.lending.domain.repository;
-import com.p2p.lending.domain.model.Loan;
-import java.util.Optional;
-
-public interface LoanRepository {
-    void save(Loan loan);
-    Optional<Loan> findById(String id);
-}
--- D5-P2P-Lending-main/src/main/java/com/p2p/lending/strategy/InterestStrategy.java
+++
@@ -1,5 +0,0 @@
-package com.p2p.lending.strategy;
-
-public interface InterestStrategy {
-    double calculateInterest(double principal, int tenorMonths);
-}
